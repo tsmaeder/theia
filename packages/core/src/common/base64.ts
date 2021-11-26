d4d93f5f19fbd02ab0f298ee04d1157b19f78b39
Thomas Mäder <tmader@redhat.com>
2021-11-26 14:35:06 +0100

    Micro-optimize encoding algorithm
    Signed-off-by: Thomas Mäder <tmader@redhat.com>

--- packages/core/src/common/base64.ts
+++ packages/core/src/common/base64.ts
@@ -16,72 +16,16 @@
 
 import { BinaryBuffer } from './buffer';
 
+/**
+ * This module allows to encode byte arrays to string and vice versa.
+ * Just using BinaryBuffer.toString() is inefficient for some reason.
+ * Building the string by hand is inefficient because of string concatenation.
+ * The approach chosen is to base 64-encode ever byte triplet and add 35 to
+ * every resulting 6-bit value to makes sure the value represent printable
+ * ASCII characters. This seems to make the underlying transport layers behave
+ * properly for some reason.
+ */
 export namespace Base64 {
-    const CODE_CHARS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/';
-
-    export function encode2(bytes: Uint8Array): string {
-        let result: string = '';
-
-        for (let i = 0; i < Math.ceil(bytes.length / 3); i++) {
-            result += encodeBytes(bytes[i * 3], bytes[(i * 3) + 1], bytes[(i * 3) + 2]);
-        }
-
-        return result;
-    }
-
-    function encodeBytes(first: number, second?: number, third?: number): string {
-        const triByte = (first << 16) + (second ? (second << 8) : 0) + (third ? third : 0);
-
-        let result = '' + CODE_CHARS.charAt((triByte >> 18) % 64);
-        result += CODE_CHARS.charAt((triByte >> 12) % 64);
-        if (typeof second === 'number') {
-            result += CODE_CHARS.charAt((triByte >> 6) % 64);
-            if (typeof third === 'number') {
-                result += CODE_CHARS.charAt((triByte) % 64);
-            } else {
-                result += '=';
-            }
-        } else {
-            result += '==';
-        }
-
-        return result;
-    }
-
-    export function decode2(encoded: string): Uint8Array {
-        if (encoded.length === 0) {
-            return new Uint8Array(0);
-        }
-        if (encoded.length % 4 !== 0) {
-            throw new Error(`encoded string length must be a multiple of 4, but is ${encoded.length}`);
-        }
-        let arrayLength = (encoded.length / 4) * 3;
-        if (encoded.charAt(encoded.length - 1) === '=') {
-            arrayLength--;
-        }
-        if (encoded.charAt(encoded.length - 2) === '=') {
-            arrayLength--;
-        }
-
-        const result = new Uint8Array(arrayLength);
-
-        for (let i = 0; i < encoded.length; i += 4) {
-            const quadruplet = (charToIndex(encoded.charAt(i)) << 18) + (charToIndex(encoded.charAt(i + 1)) << 12) + (charToIndex(encoded.charAt(i + 2)) << 6)
-                + (charToIndex(encoded.charAt(i + 3)));
-
-            const writeIndex = i / 4 * 3;
-            result[writeIndex] = quadruplet >> 16 % 256;
-            if (writeIndex < arrayLength - 1) {
-                result[writeIndex + 1] = quadruplet >> 8 % 256;
-                if (writeIndex < arrayLength - 2) {
-                    result[writeIndex + 2] = quadruplet % 256;
-                }
-            }
-        }
-
-        return result;
-    }
-
     export function decode(encoded: string): Uint8Array {
         if (encoded.length === 0) {
             return new Uint8Array(0);
@@ -108,11 +52,11 @@ export namespace Base64 {
                 + (v(source[i + 3]));
 
             const writeIndex = i / 4 * 3;
-            result[writeIndex] = quadruplet >> 16 % 256;
+            result[writeIndex] = quadruplet >> 16 & 255;
             if (writeIndex < arrayLength - 1) {
-                result[writeIndex + 1] = quadruplet >> 8 % 256;
+                result[writeIndex + 1] = quadruplet >> 8 & 255;
                 if (writeIndex < arrayLength - 2) {
-                    result[writeIndex + 2] = quadruplet % 256;
+                    result[writeIndex + 2] = quadruplet & 255;
                 }
             }
         }
@@ -120,51 +64,40 @@ export namespace Base64 {
         return result;
     }
 
-    function charToIndex(c: string): number {
-        const index = CODE_CHARS.indexOf(c);
-        return index < 0 ? 0 : index;
-    }
-
     export function encode(bytes: Uint8Array): string {
         const resultLength = Math.ceil(bytes.length / 3) * 4;
         const res = new Uint8Array(resultLength);
 
-        for (let i = 0; i < Math.ceil(bytes.length / 3); i++) {
-            res.set(encodeBytes2(bytes[i * 3], bytes[(i * 3) + 1], bytes[(i * 3) + 2]), i * 4);
+        let readIndex = 0;
+        let writeIndex = 0;
+        const sourceLength = bytes.length;
+        while (writeIndex < resultLength) {
+            let triplet = bytes[readIndex] << 16;
+
+            if (readIndex + 1 < sourceLength) {
+                triplet += bytes[readIndex + 1] << 8;
+                if (readIndex + 2 < sourceLength) {
+                    triplet += bytes[readIndex + 2];
+                }
+            }
+
+            res[writeIndex] = 35 + ((triplet >> 18) & 63);
+            res[writeIndex + 1] = 35 + ((triplet >> 12) & 63);
+            if (readIndex + 1 < sourceLength) {
+                res[writeIndex + 2] = (35 + ((triplet >> 6) & 63));
+                if (readIndex + 2 < sourceLength) {
+                    res[writeIndex + 3] = (35 + ((triplet) & 63));
+                } else {
+                    res[writeIndex + 3] = 34;
+                }
+            } else {
+                res[writeIndex + 2] = 34;
+                res[writeIndex + 3] = 34;
+            }
+            readIndex += 3;
+            writeIndex += 4;
         }
 
         return BinaryBuffer.wrap(res).toString();
     }
-
-    function encodeBytes2(first: number, second?: number, third?: number): number[] {
-        const triByte = (first << 16) + (second ? (second << 8) : 0) + (third ? third : 0);
-
-        const res: number[] = [35 + ((triByte >> 18) % 64)];
-        res.push(35 + ((triByte >> 12) % 64));
-        if (typeof second === 'number') {
-            res.push(35 + ((triByte >> 6) % 64));
-            if (typeof third === 'number') {
-                res.push(35 + ((triByte) % 64));
-            } else {
-                res.push(34);
-            }
-        } else {
-            res.push(34);
-            res.push(34);
-        }
-
-        return res;
-    }
 }
-
-// const data = new Uint8Array(1300000).fill(72);
-const data = new Uint8Array([0, 1, 2, 3]);
-const start = Date.now();
-
-const encoded2 = Base64.encode(data);
-const redecoded = Base64.decode(encoded2);
-
-console.log(`size= ${redecoded.length}`);
-
-const end = Date.now();
-console.log(end - start);
