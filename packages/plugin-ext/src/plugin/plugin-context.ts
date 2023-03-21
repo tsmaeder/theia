db1ff8eb392c3d1fb3f85a5a35577557da00507c
Zakaria Diabi <d.zaki2396@gmail.com>
2023-03-21 16:14:55 -0400

    feat(Testing API): Migration of the PluginExt part of the VS Code Test API
    Project lead: Kevin Clapperton
    Developers: Zakaria Diabi, Mathieu Bussières
    Testers: Kevin Clapperton, Théo St-Denis
    
    Signed-off-by: Zakaria Diabi  <d.zaki2396@gmail.com>
    Signed-off-by: Mathieu Bussières <mathblin@hotmail.com>
    Signed-off-by: Théo St-Denis <theo.st-denis@polymtl.ca>
    Signed-off-by: Kevin Clapperton <keclapperton@gmail.com>
    
    Co-authored-by: Mathieu Bussières <mathblin@hotmail.com>
    Co-authored-by: Théo St-Denis <theo.st-denis@polymtl.ca>
    Co-authored-by: Kevin Clapperton <keclapperton@gmail.com>

--- packages/plugin-ext/src/plugin/plugin-context.ts
+++ packages/plugin-ext/src/plugin/plugin-context.ts
@@ -199,7 +199,13 @@ import {
     DocumentPasteEdit,
     ExternalUriOpenerPriority,
     EditSessionIdentityMatch,
-    TerminalOutputAnchor
+    TerminalOutputAnchor,
+    TestResultState,
+    CoveredCount,
+    FileCoverage,
+    StatementCoverage,
+    BranchCoverage,
+    FunctionCoverage
 } from './types-impl';
 import { AuthenticationExtImpl } from './authentication-ext';
 import { SymbolKind } from '../common/plugin-api-rpc-model';
@@ -251,6 +257,7 @@ import { NotebookRenderersExtImpl } from './notebook/notebook-renderers';
 import { NotebookKernelsExtImpl } from './notebook/notebook-kernels';
 import { NotebookDocumentsExtImpl } from './notebook/notebook-documents';
 import { NotebookEditorsExtImpl } from './notebook/notebook-editors';
+import { TestingExtImpl } from './tests';
 
 export function createAPIFactory(
     rpc: RPCProtocol,
@@ -298,6 +305,7 @@ export function createAPIFactory(
     const customEditorExt = rpc.set(MAIN_RPC_CONTEXT.CUSTOM_EDITORS_EXT, new CustomEditorsExtImpl(rpc, documents, webviewExt, workspaceExt));
     const webviewViewsExt = rpc.set(MAIN_RPC_CONTEXT.WEBVIEW_VIEWS_EXT, new WebviewViewsExtImpl(rpc, webviewExt));
     const telemetryExt = rpc.set(MAIN_RPC_CONTEXT.TELEMETRY_EXT, new TelemetryExtImpl());
+    const testingExt = rpc.set(MAIN_RPC_CONTEXT.TESTING_EXT, new TestingExtImpl(rpc, commandRegistry, editorsAndDocumentsExt));
     rpc.set(MAIN_RPC_CONTEXT.DEBUG_EXT, debugExt);
 
     return function (plugin: InternalPlugin): typeof theia {
@@ -995,6 +1003,9 @@ export function createAPIFactory(
         // Tests API (@stubbed)
         // The following implementation is temporarily `@stubbed` and marked as such under `theia.d.ts`
         const tests: typeof theia.tests = {
+            // createTestController(provider: string, label: string, refreshHandler?: (token: theia.CancellationToken) => Thenable<void> | void) {
+            //     return testingExt.createTestController(provider, label, refreshHandler);
+            // },
             createTestController(
                 provider,
                 controllerLabel: string,
@@ -1013,6 +1024,18 @@ export function createAPIFactory(
                     dispose: () => undefined,
                 };
             },
+            createTestObserver() {
+                return testingExt.createTestObserver();
+            },
+            runTests(provider: theia.TestRunRequest) {
+                return testingExt.runTests(provider);
+            },
+            get onDidChangeTestResults() {
+                return testingExt.onResultsChanged;
+            },
+            get testResults() {
+                return testingExt.results;
+            }
         };
         /* End of Tests API */
 
@@ -1390,7 +1413,13 @@ export function createAPIFactory(
             DocumentPasteEdit,
             ExternalUriOpenerPriority,
             TerminalQuickFixType,
-            EditSessionIdentityMatch
+            EditSessionIdentityMatch,
+            TestResultState,
+            CoveredCount,
+            FileCoverage,
+            StatementCoverage,
+            BranchCoverage,
+            FunctionCoverage
         };
     };
 }
